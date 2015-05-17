cf5a8afa68bbe0e3918685495507d0a97b34a003
Oliver Hader <oliver@typo3.org>
2015-05-17 11:04:17 +0200

    [TASK] Integrate routing for both AngularJS and Web Components

--- www/angularjs/src/app.js
+++ www/angularjs/src/app.js
@@ -1,4 +1,32 @@
-angular.module('wecan', ['bootstrap'])
+angular.module('wecan', ['bootstrap', 'ngRoute'])
+.config(
+['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'homeCtrl'
+    })
+    .when('/map', {
+        templateUrl: 'partials/map.html',
+        controller: 'mapCtrl'
+    })
+    .when('/communication', {
+        templateUrl: 'partials/communication.html',
+        controller: 'communicationCtrl'
+    })
+    .otherwise({
+        redirectTo: '/home'
+    });
+}])
 .controller('indexCtrl', function($scope) {
+
+})
+.controller('homeCtrl', function($scope) {
+
+})
+.controller('mapCtrl', function($scope) {
+
+})
+.controller('communicationCtrl', function($scope) {
 
 });
