dcd89b08954072772e92f15908e5657cc0b2cc6d
Oliver Hader <oliver@typo3.org>
2015-05-31 23:38:04 +0200

    [TASK] Add debugging possibilities to Angular JS bootstrap-navbar

--- www/angularjs/src/bootstrap/module.js
+++ www/angularjs/src/bootstrap/module.js
@@ -7,6 +7,7 @@ angular.module('bootstrap', [])
 			return {
 				pre: function(scope, tElement, tAttrs, controller) {
                     scope.$navbar = tElement;
+                    scope.debug('bootstrapNavbar:compile.pre');
 				},
 				post: function(scope, iElement, iAttrs, controller) {
 					$http.get(iAttrs.src).success(function(data) {
@@ -14,15 +15,23 @@ angular.module('bootstrap', [])
 						iElement.html(data);
 						$compile(iElement.contents())(scope);
 					});
+                    scope.debug('bootstrapNavbar:compile.post');
 				}
 			}
 		},
 		controller: function($scope, $compile, $http) {
+            $scope.enableDebugging = true;
 			$scope.brand = '[undefined]';
 			$scope.items = [];
 			$scope.link = function(target) {
 				return '#/' + target;
 			};
+            $scope.debug = function(message) {
+                if ($scope.enableDebugging) {
+                    console.log(message);
+                }
+            };
+            $scope.debug('bootstrapNavbar:controller');
 		}
 	};
 })
@@ -33,6 +42,7 @@ angular.module('bootstrap', [])
 		require: '^bootstrapNavbar',
 		link: function($scope, element, attrs, controller) {
 			$scope.brand = attrs.name || $scope.brand;
+            $scope.debug('bootstrapNavbarBrand:link');
 		}
 	};
 })
@@ -48,6 +58,7 @@ angular.module('bootstrap', [])
                 pre: function(scope, iElement, iAttrs, controller) {
                     scope.index = scope.items.length;
                     scope.items.push({ name: iAttrs.name, target: iAttrs.target, index: scope.index });
+                    scope.debug('bootstrapNavbarItem:compile.pre');
                 }
             }
         }
