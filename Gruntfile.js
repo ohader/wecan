c9e99095bfc14eba4d7ced848c8cf295cc402f30
Oliver Hader <oliver@typo3.org>
2015-05-31 14:49:27 +0200

    [TASK] Ease bower components staging

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,19 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		copy: {
-			files: {
-				files: [
-					{expand: true, cwd: './bower_components/angularjs/', src: ['*.js', '*.css'], dest: 'www/angularjs/src/vendor/angularjs/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/jquery/dist/', src: ['**'], dest: 'www/angularjs/src/vendor/jquery/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/bootstrap/dist/', src: ['**'], dest: 'www/angularjs/src/vendor/bootstrap/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/angular-route/', src: ['**'], dest: 'www/angularjs/src/vendor/angular-route/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/angular-google-maps/dist/', src: ['**'], dest: 'www/angularjs/src/vendor/angular-google-maps/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/lodash/dist/', src: ['**'], dest: 'www/angularjs/src/vendor/lodash/', filter: 'isFile'},
+    grunt.initConfig({
+        bower: {
+            export: {
+                base: 'bower_components',
+                dest: 'www/vendor',
+                options: {
+                    checkExistence: true,
+                    debugging: true
+                }
+            }
+        }
+    });
 
-					{expand: true, cwd: './bower_components/webcomponentsjs/', src: ['*.js'], dest: 'www/webcomponents/src/vendor/webcomponents/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/polymer/', src: ['*', 'src/**'], dest: 'www/webcomponents/src/vendor/polymer/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/jquery/dist/', src: ['**'], dest: 'www/webcomponents/src/vendor/jquery/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/bootstrap/dist/', src: ['**'], dest: 'www/webcomponents/src/vendor/bootstrap/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/app-router/', src: ['**'], dest: 'www/webcomponents/src/vendor/app-router/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/google-apis/', src: ['**'], dest: 'www/webcomponents/src/vendor/google-apis/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/google-map/', src: ['**'], dest: 'www/webcomponents/src/vendor/google-map/', filter: 'isFile'},
-					{expand: true, cwd: './bower_components/iron-jsonp-library/', src: ['**'], dest: 'www/webcomponents/src/vendor/iron-jsonp-library/', filter: 'isFile'},
-				]
-			}
-		}
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-copy');
-
-	grunt.registerTask('default', ['copy']);
+    grunt.loadNpmTasks('main-bower-files');
+    grunt.registerTask('default', ['bower']);
 
 };
